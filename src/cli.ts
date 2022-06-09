ffa56c6f8ae79991c5d3a2d257b361455666c5d9
Maiko Tan <maiko.tan.coding@gmail.com>
2022-06-09 15:33:38 +0800

    refactor: move handler to utils

--- src/cli.ts
+++ src/cli.ts
@@ -1,46 +1,7 @@
 #!/bin/env node
-import path from "path";
+import yargs, { Argv } from "yargs";
 
-import fs from "fs-extra";
-import glob, { Options as GlobOptions } from "fast-glob";
-import yargs, { ArgumentsCamelCase, Argv, string } from "yargs";
-
-import * as pug from "./utils";
-
-interface Options extends GlobOptions {
-  file?: string;
-  output?: string;
-  o?: string;
-  "output-dir"?: string;
-  d?: string;
-  ext?: string;
-}
-
-export const handler = async (argv: ArgumentsCamelCase<Options>) => {
-  const { file } = argv;
-
-  if (!file) {
-    throw new Error("Invalid file path: " + file);
-  }
-
-  const files = await glob(file, { dot: true, ...argv });
-  if (files.length === 1) {
-    // if there is only one file, then the |output| option should functional.
-    let output = argv.output || pugName(files[0], argv.ext);
-    if (argv.outputDir) {
-      output = path.join(argv.outputDir, path.basename(output));
-    }
-    await convert(files[0], output);
-  } else {
-    // otherwise the |output-dir| option should functional.
-    const outputDir = argv.outputDir || ".";
-    await Promise.all(
-      files.map(async (file) => {
-        convert(file, path.join(outputDir, pugName(file)));
-      })
-    );
-  }
-};
+import { convert } from "./utils";
 
 const cli = yargs(process.argv.slice(2))
   .scriptName("html2pug-glob")
@@ -71,24 +32,9 @@ const cli = yargs(process.argv.slice(2))
           default: "pug",
         });
     },
-    handler
+    convert
   )
   .help()
   .alias("h", "help")
   .version()
   .alias("v", "version").argv;
-
-const pugName = (file: string, ext = "pug") =>
-  file.replace(/\.\w+$/, "." + ext);
-
-/**
- * convert html to pug
- */
-export const convert = async (file: string, output: string) => {
-  if (file) {
-    const pugText = await pug.fromHtmlFile(file);
-    await fs.outputFile(output, pugText);
-  } else {
-    throw new Error(`${file} is not a valid file name`);
-  }
-};
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
+import path from "path";
+
 import fs from "fs-extra";
+import glob, { Options as GlobOptions } from "fast-glob";
 import html2pug from "html2pug";
+import { ArgumentsCamelCase } from "yargs";
 
 /**
  * convert HTML to pug
@@ -39,3 +43,53 @@ export async function fromHtmlFile(file: string): Promise<string> {
 function fixupHtml2pugErrorIndent(badPug: string): string {
   return badPug.replace(/\| \n/g, "| ");
 }
+
+export interface Options extends GlobOptions {
+  file?: string;
+  output?: string;
+  o?: string;
+  "output-dir"?: string;
+  d?: string;
+  ext?: string;
+}
+
+export const convert = async (argv: ArgumentsCamelCase<Options>) => {
+  const { file } = argv;
+
+  if (!file) {
+    throw new Error("Invalid file path: " + file);
+  }
+
+  const files = await glob(file, { dot: true, ...argv });
+  if (files.length === 1) {
+    // if there is only one file, then the |output| option should functional.
+    let output = argv.output || pugName(files[0], argv.ext);
+    if (argv.outputDir) {
+      output = path.join(argv.outputDir, path.basename(output));
+    }
+    await convertFile(files[0], output);
+  } else {
+    // otherwise the |output-dir| option should functional.
+    const outputDir = argv.outputDir || ".";
+    await Promise.all(
+      files.map(async (file) => {
+        convertFile(file, path.join(outputDir, pugName(file)));
+      })
+    );
+  }
+};
+
+const pugName = (file: string, ext = "pug") =>
+  file.replace(/\.\w+$/, "." + ext);
+
+/**
+ * convert html to pug
+ */
+export const convertFile = async (file: string, output: string) => {
+  if (file) {
+    const pugText = await fromHtmlFile(file);
+    await fs.outputFile(output, pugText);
+  } else {
+    throw new Error(`${file} is not a valid file name`);
+  }
+};
